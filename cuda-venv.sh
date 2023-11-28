cc94bd53fa48d51e7e9305686042bd93851a46e5
whitgroves <whitney.groves@gmail.com>
2023-11-28 17:56:22 -0600

    v18

--- cuda-venv.sh
+++ cuda-venv.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# manually adding the extra index to requirements.txt is tedious, so use this to setup the venv instead
+
 python3 -m venv .cuda &&
 source ./.cuda/bin/activate &&
-python3 -m pip install -U --extra-index-url=https://pypi.nvidia.com pip cudf-cu12 dask-cudf-cu12 tensorflow[and-cuda] scikit-learn ipykernel pandas matplotlib
+python3 -m pip install -U --extra-index-url=https://pypi.nvidia.com pip cudf-cu12 dask-cudf-cu12 tensorflow[and-cuda] scikit-learn ipykernel pandas matplotlib lightgbm xgboost
